1c93ca6821cf2b4f4697dc2bccf4702d041082b1
kartikagarwal <54303241+kartikagarwal@users.noreply.github.com>
2023-01-22 23:25:07 +0530

    Add files via upload

---
+++ fullstack/src/main/java/com/pune/engr/fullstack/model/User.java
@@ -0,0 +1,48 @@
+package com.pune.engr.fullstack.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name ;
+    private String user_name;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String email;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+}
